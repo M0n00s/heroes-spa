1b5f94bbf2119f6c5a3a419e3330c5c14ddd82b4
M0n00s <programacion.buffalo@gmail.com>
2022-06-15 23:10:37 -0400

    context login logout

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../auth/context/AuthContext";
 
 export const Navbar = () => {
 	const navigate = useNavigate();
+	const { authState, logout } = useContext(AuthContext);
 
 	const handleLogOut = () => {
+		logout();
 		navigate("/login", {
 			replace: true,
 		});
@@ -47,7 +51,7 @@ export const Navbar = () => {
 			<div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
 				<ul className="navbar-nav ml-auto">
 					<span className="nav-item nav-link text-primary">
-						Abdel
+						{authState.user?.name}
 					</span>
 					<button
 						onClick={handleLogOut}
